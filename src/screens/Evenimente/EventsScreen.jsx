ae878b7a8ba4b988a829d00b92aad695b10aadd2
MariaP78 <maria_minodora07@yahoo.com>
2022-01-04 01:24:45 +0200

    informatii-in 2 col egale pag cu noutati si events

--- src/screens/Evenimente/EventsScreen.jsx
+++ src/screens/Evenimente/EventsScreen.jsx
@@ -8,39 +8,57 @@ import {
     Th,
     Td,
     TableCaption,
+    Text,
+    Flex,
+    Heading
 } from '@chakra-ui/react'
 
 export default function EventsScreen() {
     return (
         <ScreenTemplate
             title={"Noutăți & Evenimente"}
-            mainText={"Aici va aparea calendarul evenimentelor..."}
+            mainText={""}
             sections={[
                 {
                     component: (
-                        <Table variant='striped'>
-                            <TableCaption>Sectiunea de Noutati</TableCaption>
-                            <Thead>
-                                <Tr>
-                                    <Th>De interes pentru</Th>
-                                    <Th>Anunt</Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                    <Td>Student</Td>
-                                    <Td>A aparut structura anului universitar</Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Student</Td>
-                                    <Td>A aparut orarul semestrului 2</Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Cadru didactic</Td>
-                                    <Td>Au aparut listele cu studentii pentru fiecare specializare</Td>
-                                </Tr>
-                            </Tbody>
-                        </Table>
+                        <Flex alignItems={"center"} justifyContent={"center"}>
+                            <Flex flexDir="row" w="100vh">
+                                <Flex flexDir="column" w="60vh" marginRight="50">
+                                    <Heading fontSize={"24"}>Noutăți</Heading>
+                                    <br />
+                                    <Table variant='striped'>
+                                        <TableCaption>Secțiunea de Noutăți</TableCaption>
+                                        <Thead>
+                                            <Tr>
+                                                <Th>De interes pentru</Th>
+                                                <Th>Anunț</Th>
+                                            </Tr>
+                                        </Thead>
+                                        <Tbody>
+                                            <Tr>
+                                                <Td>Student</Td>
+                                                <Td>A apărut structura anului universitar</Td>
+                                            </Tr>
+                                            <Tr>
+                                                <Td>Student</Td>
+                                                <Td>A apărut orarul semestrului 2</Td>
+                                            </Tr>
+                                            <Tr>
+                                                <Td>Cadru didactic</Td>
+                                                <Td>Au apărut listele cu studenții pentru fiecare specializare</Td>
+                                            </Tr>
+                                        </Tbody>
+                                    </Table>
+                                </Flex>
+                                <Flex flexDir="column" w="60vh">
+                                    <Heading fontSize={"24"}>Evenimente</Heading>
+                                    <br />
+                                    <Text>Aici se va afisa calendarul evenimentelor...</Text>
+                                </Flex>
+                            </Flex>
+                        </Flex>
+
+
                     )
                 },
             ]}
