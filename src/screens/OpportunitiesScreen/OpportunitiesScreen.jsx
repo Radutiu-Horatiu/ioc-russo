fbe22b1f48c9fd02f53e41e1ca7ba2a7c9894b92
Mihai Oancea <mihai.oancea@gutenbergcerts.com>
2022-01-06 21:43:12 +0200

    oportunitati

--- src/screens/OpportunitiesScreen/OpportunitiesScreen.jsx
+++ src/screens/OpportunitiesScreen/OpportunitiesScreen.jsx
@@ -3,41 +3,47 @@ import React from "react";
 import LOGO from "../../assets/russo_logo.png";
 import MyCarousel from "../../components/MyCarousel/MyCarousel";
 import ScreenTemplate from "../../components/ScreenTemplate";
+import { List, ListItem, ListIcon, OrderedList, UnorderedList } from "@chakra-ui/react";
+import { MdCheckCircle } from "react-icons/md";
 export default function OpportunitiesScreen() {
 	return (
+		/*
+		• Departamente de resurse umane și dezvoltare organizațională din organizaţii private, multinaționale, de stat şi non-guvernamentale
+• Firme de consultanţă în domeniul resurselor umane din ţară şi străinătate
+• Institute de cercetare privind munca, resursele umane, sănătatea organizaţională
+• Instituţii educaţionale din sistemul preuniversitar şi universitar
+• Agenţii şi asociaţii naţionale şi internaţionale în domeniu (Masteratul este construit în
+acord cu principiile Asociaţiei Europene de Psihologia Muncii şi Organizaţională – EAWOP)
+
+		*/
 		<ScreenTemplate
 			title={"Oportunitati profesionale"}
-			mainText={"Acesta este subtext"}
 			sections={[
 				{
-					title: "Section1 title",
-					description: "Section1 desc",
 					component: (
-						<Flex>
-							<Text>Content sesction1</Text>
-						</Flex>
-					),
-				},
-				{
-					title: "Section2 title",
-					description: "Section2 desc",
-					component: (
-						<MyCarousel
-							slides={[
-								// 1
-								<Flex align={"center"} justify={"center"} p={"5vh"} bgColor={"purple.200"}>
-									<Image src={LOGO} h="50vh" objectFit={"cover"} />
-								</Flex>,
-								// 2
-								<Flex align={"center"} justify={"center"}>
-									<Image src={LOGO} h="50vh" objectFit={"cover"} />
-								</Flex>,
-								// 3
-								<Flex align={"center"} justify={"center"}>
-									<Image src={LOGO} h="50vh" objectFit={"cover"} />
-								</Flex>,
-							]}
-						/>
+						<List spacing={6} fontSize={20}>
+							<ListItem>
+								<ListIcon as={MdCheckCircle} color="green.500" />
+								Departamente de resurse umane și dezvoltare organizațională din organizaţii private, multinaționale, de stat şi non-guvernamentale
+							</ListItem>
+							<ListItem>
+								<ListIcon as={MdCheckCircle} color="green.500" />
+								Firme de consultanţă în domeniul resurselor umane din ţară şi străinătate
+							</ListItem>
+							<ListItem>
+								<ListIcon as={MdCheckCircle} color="green.500" />
+								Institute de cercetare privind munca, resursele umane, sănătatea organizaţională
+							</ListItem>
+							<ListItem>
+								<ListIcon as={MdCheckCircle} color="green.500" />
+								Instituţii educaţionale din sistemul preuniversitar şi universitar
+							</ListItem>
+							<ListItem>
+								<ListIcon as={MdCheckCircle} color="green.500" />
+								Agenţii şi asociaţii naţionale şi internaţionale în domeniu (Masteratul este construit în acord cu principiile Asociaţiei Europene de Psihologia
+								Muncii şi Organizaţională - EAWOP)
+							</ListItem>
+						</List>
 					),
 				},
 			]}
